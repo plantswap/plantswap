8e81b284ac6470b569e42df0d1a72d099b61c744
MRzsztk <rzeszutek.malgorzata@gmail.com>
2020-07-08 10:56:37 +0200

    fixed profile edit

--- routes/index.js
+++ routes/index.js
@@ -13,34 +13,34 @@ const loginCheck = () => {
   };
 };
 
-// router.get('profile/edit/:userId', (req, res) => {
-//   const user = req.user;
-//   User.findById(req.params.userId)
-//     .then(userfound => {
-//       res.render('userEdit', { user: user })
-//     }).catch(err => {
-//       console.log(err);
-//     });
-// })
+router.get('/profile/:userId/edit', (req, res) => {
+  const user = req.user;
+  User.findById(req.params.userId)
+    .then(userfound => {
+      res.render('userEdit', { user: user })
+    }).catch(err => {
+      console.log(err);
+    });
+})
 
-// router.post('profile/edit/:userId', (req, res) => {
-//   const user = req.user;
-//   const {country, city, zip, street, house} = req.body;
-//   User.findByIdAndUpdate(req.params.userId, {
-//     name,
-//     country, 
-//     city, 
-//     zip, 
-//     street, 
-//     house
-//   })
-//     .then(userupdated => {
-//       res.redirect(`/profile`);
-//     })
-//     .catch(err => {
-//       console.log(err);
-//     });
-// })
+router.post('/profile/edit/:userId', (req, res) => {
+  const user = req.user;
+console.log(user)
+  const {country, city, zip, street, house} = req.body;
+  User.findByIdAndUpdate(req.params.userId, {
+    country, 
+    city, 
+    zip, 
+    street,
+    house
+  })
+    .then(userupdated => {
+      res.redirect(`/profile`);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+})
 
 router.get('/', (req, res, next) => {
   const user = req.user;
